617d3c92587954ae78b76515abe4a7bf556dc734
Naim Morshed <morshed.naim13@gmail.com>
2022-03-02 22:40:41 -0800

    migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,4 +11,10 @@
 @section('content')
     <h1 class="text-2xl">Welcome</h1>
     <p class="text-3xl font-bold">This is learning basics of Laravel</p>
+    <span class="text-2xl font-semibold mt-10">👉Links</span>
+    <div class="mt-10 text-xl text-blue-500">
+        <a class="border border-blue-200 p-3 bg-gray-50 hover:bg-blue-100 cursor-pointer" href="/welcome">Welcome</a>
+        <a class="border border-blue-200 p-3 bg-gray-50 hover:bg-blue-100 cursor-pointer" href="/home">Home</a>
+        <a class="border border-blue-200 p-3 bg-gray-50 hover:bg-blue-100 cursor-pointer" href="/contact">Contact</a>
+    </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::get('/about', function () {
